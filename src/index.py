127ee41866e1fef906cfd2e3f0c461290b3ca978
Serafim Sukharev <simasuh@gmail.com>
2022-10-10 10:06:10 +0300

    merge start

--- src/index.py
+++ src/index.py
@@ -1,11 +1,10 @@
 import os
 import telebot
-from telebot import types
 from dotenv import load_dotenv
 from DataAccessFunctions import getCategories, getDishes
-from order import order
 
 from registration import registration
+from start import start
 
 def main():
     load_dotenv()
@@ -15,25 +14,9 @@ def main():
     def r(message):
         registration(bot, message)
 
-    @bot.message_handler(commands=['o'])
-    def o(message):
-        order(bot, message)
-
     @bot.message_handler(commands=['start'])
-    def start(message):
-        markup = types.ReplyKeyboardMarkup(row_width=2)
-        button1 = types.KeyboardButton("Сделать Заказ")
-        button2 = types.KeyboardButton("Предыдущий заказ")
-        markup.add(button1, button2)
-        bot.send_message(message.chat.id, "Здравствуйте, вы желаете сделать заказ, или просмотреть предыдущий заказ?", reply_markup=markup)
-
-    @bot.message_handler(func=lambda message: message.text is not None and '/' not in message.text)
-    def reply(message):
-        if message.text == "Сделать Заказ":
-            markup = types.ReplyKeyboardMarkup(row_width=2)
-            button1 = types.KeyboardButton("Ресторан 1")
-            markup.add(button1)
-            bot.send_message(message.chat.id, "Выберите ресторан", reply_markup=markup)
+    def s(message):
+        start(bot, message)
     
     bot.infinity_polling()
 
